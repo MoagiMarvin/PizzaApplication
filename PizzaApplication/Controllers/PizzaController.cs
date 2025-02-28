1e4b2ad6d6bcf1ccdb316015db53e423b6f7db9f
lenovo <rmoagi246@gmail.co>
2025-02-28 11:06:52 +0200

    Add project files.

---
+++ PizzaApplication/Controllers/PizzaController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+ 
+using PizzaApplication.Models;
+using System.Collections.Generic;
+
+namespace PizzaApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzasController : ControllerBase
+    {
+        // This is the in-memory list of pizzas
+        private static readonly List<Pizza> Pizzas = new List<Pizza>
+        {
+            new Pizza { Id = 1, Name = "Margherita", Description = "Classic pizza with mozzarella and tomato sauce", Price = 8.99m },
+            new Pizza { Id = 2, Name = "Pepperoni", Description = "Pizza topped with pepperoni slices", Price = 9.99m }
+        };
+
+        // Get all pizzas
+        [HttpGet]
+        public ActionResult<IEnumerable<Pizza>> GetPizzas()
+        {
+            return Ok(Pizzas);
+        }
+
+        // Get a specific pizza by id
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> GetPizza(int id)
+        {
+            var pizza = Pizzas.Find(p => p.Id == id);
+            if (pizza == null)
+            {
+                return NotFound();
+            }
+            return Ok(pizza);
+        }
+
+        // Add a new pizza
+        [HttpPost]
+        public ActionResult<Pizza> PostPizza(Pizza pizza)
+        {
+            pizza.Id = Pizzas.Count + 1; // Auto-generate ID for the new pizza
+            Pizzas.Add(pizza);
+            return CreatedAtAction(nameof(GetPizza), new { id = pizza.Id }, pizza);
+        }
+    }
+}
---
+++ PizzaApplication/Services/PizzaService.cs
@@ -0,0 +1,40 @@
+﻿ 
+using PizzaApplication.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PizzaApplication.Services
+{
+    public interface IPizzaService
+    {
+        IEnumerable<Pizza> GetAllPizzas();
+        Pizza GetPizzaById(int id);
+        Pizza AddPizza(Pizza pizza);
+    }
+
+    public class PizzaService : IPizzaService
+    {
+        private static readonly List<Pizza> Pizzas = new List<Pizza>
+        {
+            new Pizza { Id = 1, Name = "Margherita", Description = "Classic pizza with mozzarella and tomato sauce", Price = 8.99m },
+            new Pizza { Id = 2, Name = "Pepperoni", Description = "Pizza topped with pepperoni slices", Price = 9.99m }
+        };
+
+        public IEnumerable<Pizza> GetAllPizzas()
+        {
+            return Pizzas;
+        }
+
+        public Pizza GetPizzaById(int id)
+        {
+            return Pizzas.FirstOrDefault(p => p.Id == id);
+        }
+
+        public Pizza AddPizza(Pizza pizza)
+        {
+            pizza.Id = Pizzas.Count + 1;  // Auto-generate ID for the new pizza
+            Pizzas.Add(pizza);
+            return pizza;
+        }
+    }
+}
